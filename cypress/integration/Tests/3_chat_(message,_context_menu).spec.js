bb599c58db1e7fe84d6a8b89a75c398d01c8d601
Екатерина Антонова <karafiat@mail.ru>
2022-05-03 19:45:25 +0700

    test for context menu (forward)

--- cypress/integration/Tests/3_chat_(message,_context_menu).spec.js
+++ cypress/integration/Tests/3_chat_(message,_context_menu).spec.js
@@ -9,84 +9,119 @@ function typingAMessage() {
     cy.get("#messageFieldValue")
     .type(`${messageForTyping}{enter}`);
 }
+function openContextMenu(messageTag, messageText, menuItem) {
+  cy.get(messageTag)
+  .contains(messageText)
+  .rightclick({ force: true });
+  cy.get(".StyledContextMenuBG_s460k0").should("be.visible");
+  cy.get(".react-contexify__item__content")
+      .contains(menuItem)
+      .click({ force: true });
+}
+function closeReplyHeader(){
+      cy.get('div[class="ReplyCloseBtn_rkjerc6"]')
+      .children('button[class="StyleButton_s1oa7yi9"]')
+      .click();
+}
 
 
 describe("User's chat and message's context menu", () => {
+  beforeEach(() => {
+    // авторизация. Потом перепишу в отдельную простую функцию для запуска в 1 строку
+    cy.viewport(1800, 950).visit(tezroData.urls.startUrl);
+    cy.url().should("eq", tezroData.urls.startUrl);
+    cy.get(`${locators.signUpLocators.startPage.signInButton}`).click();
+    cy.url().should("eq", tezroData.urls.signInUrl);
+    cy.get(`${signinLoc.seedPhraseInput}`).type(
+      `${tezroData.userLoginData[0].seedPhrase}`
+    ); // подумать,
+    //как прикрутить функцию Лены typeTextData
+    cy.get(`${signinLoc.seedPhraseInput}`).should(
+      "contain.value",
+      `${tezroData.userLoginData[0].seedPhrase}`
+    );
+    cy.get(`${signinLoc.loginButton}`).click().wait(1000);
+    signinLoc.pinInput.forEach((pinCount) => {
+      cy.get(`${pinCount}`).type("1");
+    });
+    signinLoc.pinInput.forEach((pinCount) => {
+      cy.focused().type("1");
+    });
+    cy.wait(2000).url().should("eq", "https://dev-web.tezro.com/");
+  });
 
-    beforeEach(()=> {
-        // авторизация. Потом перепишу в отдельную простую функцию для запуска в 1 строку
-        cy.viewport(1800, 950).visit(tezroData.urls.startUrl);
-        cy.url().should("eq", tezroData.urls.startUrl);
-        cy.get(`${locators.signUpLocators.startPage.signInButton}`).click();
-        cy.url().should("eq", tezroData.urls.signInUrl);
-        cy.get(`${signinLoc.seedPhraseInput}`)
-        .type(`${tezroData.userLoginData[0].seedPhrase}`) // подумать, 
-        //как прикрутить функцию Лены typeTextData
-        cy.get(`${signinLoc.seedPhraseInput}`)
-          .should('contain.value', `${tezroData.userLoginData[0].seedPhrase}`)
-        cy.get(`${signinLoc.loginButton}`).click().wait(1000)
-        signinLoc.pinInput.forEach(pinCount => {
-          cy.get(`${pinCount}`).type('1')
-        })
-        signinLoc.pinInput.forEach(pinCount => {
-          cy.focused().type("1")
-        })
-        cy.wait(2000).url().should("eq", "https://dev-web.tezro.com/");
-                })
+  it.skip("1. Write message in chat", () => {
+    cy.get('span[class="StyledText_sm7kffo"]')
+      .should("have.text", "Choose who you would like to write to")
+      .should("be.visible");
 
-    it.skip("1. Write message in chat", () => {
-        cy.get('span[class="StyledText_sm7kffo"]')
-        .should("have.text", 'Choose who you would like to write to')
-        .should("be.visible");
+    // сами тесты чатов
+    cy.get("#dialogItem")
+      .first("button")
+      .should("be.visible")
+      .should("be.enabled")
+      .click({ force: true });
+    cy.get('span[class="StyledText_sm7kffo"]').should("not.exist");
+    cy.get("#messageFieldValue")
+      .should("be.visible")
+      .and("be.enabled")
+      .type(messageForTyping)
+      .should("contain.value", messageForTyping)
+      .type("{enter}");
+    cy.get(
+      'p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]'
+    )
+      .contains(messageForTyping)
+      .should("exist")
+      .and("have.text", messageForTyping);
+  }); //it1
 
-        // сами тесты чатов
-        cy.get('#dialogItem')
-        .first('button').as('chatButton') // без этого отваливалось, как detached from the DOM
-        .should('be.visible')
-        .should('be.enabled')
-        .click({force: true});
-        cy.get('span[class="StyledText_sm7kffo"]')
-        .should('not.exist');
-        cy.get("#messageFieldValue")
-        .should('be.visible').and('be.enabled')
-        .type(messageForTyping)
-        .should("contain.value", messageForTyping).type('{enter}');
-        cy.get('p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]')
-        .contains(messageForTyping)
-        .should('exist').and('have.text', messageForTyping);
-    });//it1
-
-    it("2. Message's context menu", () => {
-        cy.get('#dialogItem')
-        /* пытаюсь добраться до второго чата 
+  it("2. Message's context menu", () => {
+    //REPLY
+    cy.log("1before wait").wait(2000);
+    cy.get("#dialogItem")
+      /* пытаюсь добраться до второго чата 
         cy.get('#dialogItem').siblings('button').then(($elem) => {
             return $elem[0];
         }).click();*/
-        .should('exist')
-        .first('button')
-        .click();
+      .should("exist")
+      .first("button")
+      .click()
+      .log("2after wait");
+    typingAMessage();
+    openContextMenu(
+      `p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]`,
+      messageForTyping, "Reply"
+    );
+    //typingAMessage();
+    cy.get("#messageFieldValue")
+      .wait(1000)
+      .type(`sdfsdfsdf`)
+      .should("contain.value", "sdfsdfsdf")
+      .type("{enter}");
+    //закрываем шапку реплая в поле текста
+    closeReplyHeader();
+    // проверка, что отправилось сообщение-ответ
+    // cy.get('p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]')
+    // .contains('sdfsdfsdf');
 
-        typingAMessage();
+    //FORWARD
+    openContextMenu(
+      `p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]`,
+      messageForTyping, "Forward"
+    );
+    //лезем в первый выбор среди диалогов
+    cy.get('#dialogs-for-share-infinityScrollWrapper')
+    .should("exist").and('be.visible');
+    cy.get('div[class="StyledChatDialogMini_s1cy661g"]')
+    .first()
+    .find('button[class="StyledButtonCheckbox_s1jjp8t8 StyleButton_s1oa7yi9"]')
+    .click()
+    //галочка проставилась (в принципе, а не в конкретном поле, над конкретным ещё подумтаь)
+    cy.get(`div[style="--c1wv8s2o-0:scale(1);"]`);
+    cy.get('button[class="ChatShareBtn_c1r1uewz ChatShareBtnShare_c1ossl8p StyleButton_s1oa7yi9"]')
+    .click();
+    
 
-        cy.get('p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]')
-        .contains(messageForTyping)
-        .rightclick({force: true});
-
-        cy.get('.StyledContextMenuBG_s460k0')
-        .should("be.visible");
-        
-        cy.get('.react-contexify__item__content')
-        .contains('Reply').click({force : true});
-        
-        //typingAMessage();
-        cy.get("#messageFieldValue")
-        .type(`sdfsdfsdf`).should('contain.value', 'sdfsdfsdf').type('{enter}');
-
-        // cy.get('p[class="messageTextItem MessageClassStyle_m2dsqsi StyleText_svkxk8i"]')
-        // .contains('sdfsdfsdf');
-
-
-    }); //it2
-
-
-  });//describe
+  }); //it2
+});//describe
